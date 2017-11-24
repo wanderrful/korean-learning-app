be5341a93cb0d9294070d444827b1ed913ad13d1
Wanderrful <galois@mentlegen.com>
2017-11-24 09:36:00 +0900

    Started adding PostgreSQL support

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as pg from "pg";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./styles/index.css";
 
 
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root") as HTMLElement
-);
+const pgClient = pg.Client({});
+
+
+
+pgClient.query("", fn_initApp);
+
+
+
+function fn_initApp(): void {
+  ReactDOM.render(
+    <App />,
+    document.getElementById("root") as HTMLElement
+  );
+}
 
 
 
